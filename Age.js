32ecd6e96ee5480c7a7edc63adc364c0058de2de
Alfred Skaarup <Alfredowich@gmail.com>
2022-10-31 21:07:38 +0100

    En penis

---
+++ Age.js
@@ -0,0 +1,16 @@
+
+2. Få lortet til at trække et tal fra den const
+3. Lav tallet en variabel vi trækker fra din alders const
+4. Sæt tekster på differencen (det ser vi på bagefter)
+
+const alfredAgeInYears = 18 
+let yourAge = ?
+
+if (yourAge = 18) {
+    = "WOW vi er jo lige gamle i år!";
+  }
+
+
+
+// Hvordan skal jeg lave en udefineret værdi når man ikke kan ændre den herinde?
+  
